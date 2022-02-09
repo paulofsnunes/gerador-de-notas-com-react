0807017db5990116f12988a26db4b28302413acf
Paulo Franklin <paulofsnunes@gmail.com>
2022-02-09 11:53:47 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import ListaNotas from "./components/ListaNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaNotas notas={this.state.notas}/>
+      </section>
+    );
+  }
+}
+
+//new ListaNotas({notas:this.notas})
+export default App;
